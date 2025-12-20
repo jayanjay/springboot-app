0e2a6414fe2d71579c9ccd49e019ff4643e59ab5
jayan <jayanjai1986@gmail.com>
2025-12-20 20:23:14 +0530

    updated HelloController.java file

---
+++ src/main/java/com/tcs/angularjs/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.tcs.angularjs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
